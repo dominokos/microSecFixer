f3bce6a2c781d3c0760b02b3d372e322f83d463b
dominokos <dominik.jacob@tuhh.de>
2024-08-26 12:22:35 +0200

    Ensuring that the logging directory is not overfilled

--- microSecFixer/core/logger.py
+++ microSecFixer/core/logger.py
@@ -1,21 +1,23 @@
 """Logger implemented here. Other methods call this one when writing messages to log to have a single logger."""
 
+from logging.handlers import RotatingFileHandler
 import os
 import logging
 from datetime import date, datetime
 
-log_level = "DEBUG"
 
-numeric_level = getattr(logging, log_level.upper(), 10)
-LOG_FORMAT = "%(levelname)s %(asctime)s - %(message)s"
+log_formatter = logging.Formatter("%(asctime)s %(levelname)-8s %(message)s",
+                                  datefmt="%Y-%m-%d %H:%M:%S")  # your logging format
 LOG_PATH = "./output/logs/"
 os.makedirs(os.path.dirname(LOG_PATH), exist_ok=True)
+file_name = LOG_PATH + date.today().strftime("%b-%d-%Y") + "--" + datetime.now().strftime("%H-%M-%S") + ".log"
 
-file = "./output/logs/test.log"
-#file = LOG_PATH + date.today().strftime("%b-%d-%Y") + "--" + datetime.now().strftime("%H-%M-%S") + ".log"
+log_handler = RotatingFileHandler(file_name, mode="a", maxBytes=1024, backupCount=10, encoding="utf-8")
+log_handler.setFormatter(log_formatter)
+#file = "./output/logs/test.log"
 logger = logging.getLogger("tool")
-logging.basicConfig(filename = file, level = numeric_level, format = LOG_FORMAT)
-
+logger.addHandler(log_handler)
+logger.setLevel(logging.DEBUG) 
 
 # Set log levels of all imported modules to ERROR
 logging.getLogger("github").setLevel(logging.ERROR)
