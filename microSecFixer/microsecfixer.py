f276b9ca664a048ddaba8266211adc8f23eaef41
dominokos <dominik.jacob@tuhh.de>
2024-09-03 15:42:19 +0200

    we store the original dfd and the new for viewing and comparison separately, rules aren't always checked altogether but instead, if we can, we will check only a range of rules that might still be failing

--- microSecFixer/microsecfixer.py
+++ microSecFixer/microsecfixer.py
@@ -9,7 +9,7 @@ from datetime import datetime
 
 from microCertiSec.core.model import CModel
 import microSecFixer.core.visualizer as visualizer
-from microSecFixer.core.evaluation import find_all_violations
+from microSecFixer.core.evaluation import find_all_violations_between
 import microSecFixer.core.logger as logger
 from microSecFixer.core.rule_map import RuleMap
 import microSecFixer.tmp.tmp as temp
@@ -18,8 +18,8 @@ from microCertiSec.microCertiSec import model_api
 def get_model(model_path: str) -> CModel:
     return model_api(model_path, None)
 
-def visualize_dfd(dfd_path: str):
-    visualizer.visualize(dfd_path)
+def visualize_dfd(dfd_path: str, out_location: str):
+    visualizer.visualize(dfd_path, out_location)
 
 def insertConfigIntoTemp(config: ConfigParser):
     logger.write_log_message("Copying config file to tmp file", "debug")
@@ -42,20 +42,25 @@ def main():
     insertConfigIntoTemp(ini_config)
 
     dfd_path = temp.tmp_config.get("Repository", "dfd_path")
-    visualize_dfd(dfd_path)
+    visualize_dfd(dfd_path, "original")
 
     # calling microCertiSec to evaluate the security of the model and saving the results under output/results/{model name}
-    result_dir = find_all_violations(dfd_path)
+    result_dir = find_all_violations_between(dfd_path, 1, 26)
     results = os.listdir(result_dir)
     fixing_map = RuleMap.get_fixes()
     while(results):
         model_path = f".{dfd_path}"
         rule = results[0][:3]
         dfd_path = fixing_map[rule](get_model(model_path))
-        result_dir = find_all_violations(dfd_path)
+        rule_number = int(rule[-2:])
+        upper_bound = 26
+        if len(results) > 1:
+            next_failing_rule = results[1][:3]
+            upper_bound = min(26, int(next_failing_rule[-2:])+2)
+        result_dir = find_all_violations_between(dfd_path, rule_number, upper_bound)
         results = os.listdir(result_dir)
 
-    visualize_dfd(dfd_path)
+    visualize_dfd(dfd_path, "ideal")
     now = datetime.now()
     end_time = now.strftime("%H:%M:%S")
 
