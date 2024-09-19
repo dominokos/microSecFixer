26e7d78ec17422f958d6f04b2b0516e5ebeb8fc9
dominokos <dominik.jacob@tuhh.de>
2024-09-19 12:07:22 +0200

    Improved setup

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 from setuptools import find_packages, setup
 
+# Read the requirements from the requirements.txt file
+with open('requirements.txt') as f:
+    install_requires = f.read().splitlines()
+
 setup(
     name='microSecFixer',
     version='0.0.1',
-    packages=find_packages()
+    packages=find_packages(),
+    install_requires=install_requires
 )
